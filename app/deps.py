13a32a69caee3746754faadd6e1b4b111e1c9cdf
yajleng <lengfuyaj@gmail.com>
2025-11-08 01:16:55 -0500

    Update deps.py

--- app/deps.py
+++ app/deps.py
@@ -1,1 +1,7 @@
 
+from .core.config import get_settings
+from .clients.apisports import ApiSportsClient
+
+def get_client() -> ApiSportsClient:
+    s = get_settings()
+    return ApiSportsClient(api_key=s.apisports_key)
